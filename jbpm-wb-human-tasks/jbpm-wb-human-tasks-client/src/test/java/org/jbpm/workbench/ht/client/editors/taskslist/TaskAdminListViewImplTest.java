kiegroup/jbpm-wb

JBPM-6174: Add error count column to the task admin list (#965)

jbpm-wb-human-tasks/jbpm-wb-human-tasks-client/src/test/java/org/jbpm/workbench/ht/client/editors/taskslist/TaskAdminListViewImplTest.java
@@ -15,18 +15,34 @@
  */
 package org.jbpm.workbench.ht.client.editors.taskslist;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import com.google.gwtmockito.GwtMockitoTestRunner;
+
+import org.jboss.errai.ioc.client.api.ManagedInstance;
+import org.jbpm.workbench.common.client.list.ExtendedPagedTable;
+import org.jbpm.workbench.common.client.util.GenericErrorSummaryCountCell;
+import org.jbpm.workbench.ht.model.TaskSummary;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.uberfire.ext.services.shared.preferences.GridColumnPreference;
+import org.uberfire.ext.services.shared.preferences.GridGlobalPreferences;
+import org.uberfire.ext.widgets.table.client.ColumnMeta;
 
+import static org.jbpm.workbench.common.client.list.AbstractMultiGridView.COL_ID_ACTIONS;
 import static org.jbpm.workbench.common.client.list.AbstractMultiGridView.TAB_SEARCH;
 import static org.jbpm.workbench.ht.client.editors.taskslist.TaskAdminListViewImpl.TAB_ADMIN;
+import static org.jbpm.workbench.ht.model.TaskDataSetConstants.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyList;
 import static org.mockito.Mockito.*;
 
 @RunWith(GwtMockitoTestRunner.class)
@@ -35,9 +51,12 @@ public class TaskAdminListViewImplTest extends AbstractTaskListViewTest {
     @InjectMocks
     @Spy
     private TaskAdminListViewImpl view;
-
     @Mock
     private TaskAdminListPresenter presenter;
+    @Spy
+    private GenericErrorSummaryCountCell cellMock;
+    @Mock
+    private ManagedInstance<GenericErrorSummaryCountCell> cellInstance;
 
     @Override
     public AbstractTaskListView getView() {
@@ -54,11 +73,57 @@ public class TaskAdminListViewImplTest extends AbstractTaskListViewTest {
         return Arrays.asList(TAB_SEARCH,
                              TAB_ADMIN);
     }
+    
+    @Override
+    public List<String> getExpectedInitialColumns() {
+        return Arrays.asList(COLUMN_NAME,
+                             COLUMN_PROCESS_ID,
+                             COLUMN_STATUS,
+                             COLUMN_CREATED_ON,
+                             COLUMN_ERROR_COUNT,
+                             COL_ID_ACTIONS);
+    }
+    
+    @Override
+    public Integer getExpectedNumberOfColumns() {
+        return 16;
+    }
 
     @Before
     @Override
     public void setupMocks() {
         super.setupMocks();
         when(presenter.createAdminTabSettings()).thenReturn(filterSettings);
+        when(cellInstance.get()).thenReturn(cellMock);
+    }
+    
+    @Test
+    public void initColumnsWithTaskVarColumnsTest() {
+        final ExtendedPagedTable<TaskSummary> currentListGrid = spy(new ExtendedPagedTable<>(new GridGlobalPreferences()));
+        doAnswer(new Answer() {
+            @Override
+            public Void answer(InvocationOnMock invocationOnMock) throws Throwable {
+                final List<ColumnMeta> columns = (List<ColumnMeta>) invocationOnMock.getArguments()[0];
+                assertEquals(19, columns.size());
+                return null;
+            }
+        }).when(currentListGrid).addColumns(anyList());
+
+        ArrayList<GridColumnPreference> columnPreferences = new ArrayList<GridColumnPreference>();
+        columnPreferences.add(new GridColumnPreference("var1",
+                                                       0,
+                                                       "40"));
+        columnPreferences.add(new GridColumnPreference("var2",
+                                                       1,
+                                                       "40"));
+        columnPreferences.add(new GridColumnPreference("var3",
+                                                       1,
+                                                       "40"));
+        when(currentListGrid.getGridPreferencesStore()).thenReturn(gridPreferencesStore);
+        when(gridPreferencesStore.getColumnPreferences()).thenReturn(columnPreferences);
+
+        getView().initColumns(currentListGrid);
+
+        verify(currentListGrid).addColumns(anyList());
     }
 }

cristianonicolai/jbpm-console-ng

jbpm-wb-human-tasks/jbpm-wb-human-tasks-client/src/test/java/org/jbpm/workbench/ht/client/editors/taskslist/TaskAdminListViewImplTest.java
@@ -15,18 +15,33 @@
  */
 package org.jbpm.workbench.ht.client.editors.taskslist;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import com.google.gwtmockito.GwtMockitoTestRunner;
+import org.jboss.errai.ioc.client.api.ManagedInstance;
+import org.jbpm.workbench.common.client.list.ListTable;
+import org.jbpm.workbench.common.client.util.GenericErrorSummaryCountCell;
+import org.jbpm.workbench.ht.model.TaskSummary;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.uberfire.ext.services.shared.preferences.GridColumnPreference;
+import org.uberfire.ext.services.shared.preferences.GridGlobalPreferences;
+import org.uberfire.ext.widgets.table.client.ColumnMeta;
 
+import static org.jbpm.workbench.common.client.list.AbstractMultiGridView.COL_ID_ACTIONS;
 import static org.jbpm.workbench.common.client.list.AbstractMultiGridView.TAB_SEARCH;
 import static org.jbpm.workbench.ht.client.editors.taskslist.TaskAdminListViewImpl.TAB_ADMIN;
+import static org.jbpm.workbench.ht.model.TaskDataSetConstants.*;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyList;
 import static org.mockito.Mockito.*;
 
 @RunWith(GwtMockitoTestRunner.class)
@@ -38,6 +53,12 @@ public class TaskAdminListViewImplTest extends AbstractTaskListViewTest {
 
     @Mock
     private TaskAdminListPresenter presenter;
+
+    @Spy
+    private GenericErrorSummaryCountCell cellMock;
+
+    @Mock
+    private ManagedInstance<GenericErrorSummaryCountCell> cellInstance;
 
     @Override
     public AbstractTaskListView getView() {
@@ -55,10 +76,57 @@ public class TaskAdminListViewImplTest extends AbstractTaskListViewTest {
                              TAB_ADMIN);
     }
 
+    @Override
+    public List<String> getExpectedInitialColumns() {
+        return Arrays.asList(COLUMN_NAME,
+                             COLUMN_PROCESS_ID,
+                             COLUMN_STATUS,
+                             COLUMN_CREATED_ON,
+                             COLUMN_ERROR_COUNT,
+                             COL_ID_ACTIONS);
+    }
+
+    @Override
+    public Integer getExpectedNumberOfColumns() {
+        return 16;
+    }
+
     @Before
     @Override
     public void setupMocks() {
         super.setupMocks();
         when(presenter.createAdminTabSettings()).thenReturn(filterSettings);
+        when(cellInstance.get()).thenReturn(cellMock);
+    }
+
+    @Test
+    public void initColumnsWithTaskVarColumnsTest() {
+        final ListTable<TaskSummary> currentListGrid = spy(new ListTable<>(new GridGlobalPreferences()));
+        doAnswer(new Answer() {
+            @Override
+            public Void answer(InvocationOnMock invocationOnMock) throws Throwable {
+                final List<ColumnMeta> columns = (List<ColumnMeta>) invocationOnMock.getArguments()[0];
+                assertEquals(19,
+                             columns.size());
+                return null;
+            }
+        }).when(currentListGrid).addColumns(anyList());
+
+        ArrayList<GridColumnPreference> columnPreferences = new ArrayList<GridColumnPreference>();
+        columnPreferences.add(new GridColumnPreference("var1",
+                                                       0,
+                                                       "40"));
+        columnPreferences.add(new GridColumnPreference("var2",
+                                                       1,
+                                                       "40"));
+        columnPreferences.add(new GridColumnPreference("var3",
+                                                       1,
+                                                       "40"));
+        when(currentListGrid.getGridPreferencesStore()).thenReturn(gridPreferencesStore);
+        when(gridPreferencesStore.getColumnPreferences()).thenReturn(columnPreferences);
+
+        getView().initColumns(currentListGrid);
+
+        verify(currentListGrid).addColumns(anyList());
     }
 }